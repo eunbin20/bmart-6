27cfd3e1d79c9753cd8f9436bc4dc26d2ad6413c
zoomkoding <zoomkoding@gmail.com>
2020-08-11 21:18:45 +0900

    feat -  생성한 router 사용

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const userRouter = require("./user");
+const categoryRouter = require("./category");
+const subcategoryRouter = require("./subcategory");
+const productRouter = require("./product");
 
 router.use("/user", userRouter);
+router.use("/category", categoryRouter);
+router.use("/subcategory", subcategoryRouter);
+router.use("/product", productRouter);
 
 module.exports = router;
