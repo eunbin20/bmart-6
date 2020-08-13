8e02a61bfcfb9da2a79d746e1fb2d573fda97788
Donguk Hong <mbxd1@naver.com>
2020-08-13 14:07:16 +0900

    [#7] refactor - 라우터 > jwt 미들웨어 필요한 곳에 추가해줌

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 const { validateCreateUser } = require('../middlewares/validator');
-const { isAuthenticated } = require('../middlewares/auth');
+const { isAuthenticated, isValidJwtToken } = require('../middlewares/auth');
 
 router.post('/', validateCreateUser, userController.create); // 회원가입
 router.post('/login', isAuthenticated, userController.login);
-router.put('/', userController.update);
-router.delete('/:id', userController.delete);
+router.put('/', isValidJwtToken, userController.update);
+router.delete('/:id', isValidJwtToken, userController.delete);
 
 module.exports = router;
