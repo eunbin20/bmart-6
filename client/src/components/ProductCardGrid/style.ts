24d520d07595bf144e8210a6fd12b8fbfba29ae1
zoomkoding <zoomkoding@gmail.com>
2020-08-13 22:48:50 +0900

    feat - productCardGrid style 구현

---
+++ client/src/components/ProductCardGrid/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const CardGridContainer = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 12px;
+  row-gap: 15px;
+`;
