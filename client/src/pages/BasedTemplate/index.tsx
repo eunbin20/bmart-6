dbadb8e2eceb04447c8ac26e5c7b7edffa07c4a2
zoomkoding <zoomkoding@gmail.com>
2020-08-13 22:50:34 +0900

    feat - 여러 페이지의 default template 구현

---
+++ client/src/pages/BasedTemplate/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import * as S from './style';
+// import { Header, Footer, Divider, Loading } from '../../components';
+
+interface Props {
+  loading?: boolean;
+  internalServerError?: boolean;
+  children: React.ReactNode;
+}
+
+function BasedTemplate({
+  children,
+  loading = false,
+  internalServerError = false,
+}: Props): React.ReactElement {
+  const conditionalRender = () => {
+    // if (internalServerError)
+    //   return (
+    //     <S.InternalServerError
+    //       alt="500 Internal Server Error"
+    //       src="https://kr.object.ncloudstorage.com/bookus/internalServerError.png"
+    //     />
+    //   );
+    // if (loading) return <Loading />;
+    return children;
+  };
+  return (
+    <>
+      <S.HeaderWrapper>{/* <Header /> */}</S.HeaderWrapper>
+      <S.ChildrenWrapper>{conditionalRender()}</S.ChildrenWrapper>
+      <S.FooterWrapper>{/* <Footer /> */}</S.FooterWrapper>
+    </>
+  );
+}
+
+export default BasedTemplate;
