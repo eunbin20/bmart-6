a3eb327e2bb774e68a9b435de7b04fbd589904b2
zoomkoding <zoomkoding@gmail.com>
2020-08-13 22:52:02 +0900

    feat - Header에서 changeSort 함수 구현

--- client/src/pages/Subcategory/index.tsx
+++ client/src/pages/Subcategory/index.tsx
@@ -10,11 +10,21 @@ import { ACTION_FETCH_PRODUCTS } from '../../commons/constants/string';
 function SubCategory(): React.ReactElement {
   const [state, setState] = useProducts({ subcategoryId: 1 });
 
+  function changeSort(sortBy: ProductSort) {
+    setState({
+      type: ACTION_FETCH_PRODUCTS,
+      data: {
+        subcategoryId: 1,
+        ...(sortBy && { sortBy }),
+      },
+    });
+  }
+
   const { products } = state;
   if (!products || !products) {
     return (
       <SubcategoryTemplate
-        productCardGridHeader={<ProductCardGridHeader />}
+        productCardGridHeader={<ProductCardGridHeader changeSort={changeSort} />}
         productCardGrid={<></>}
       />
     );
@@ -23,7 +33,7 @@ function SubCategory(): React.ReactElement {
   return (
     <SubcategoryTemplate
       //   categoryBanner={<SubCategoryBanner />}
-      productCardGridHeader={<ProductCardGridHeader />}
+      productCardGridHeader={<ProductCardGridHeader changeSort={changeSort} />}
       productCardGrid={<ProductCardGrid products={products} />}
     />
   );
