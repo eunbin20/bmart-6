da4c1a7ddb1ea4be8a2786371dde84ce1b334858
Donguk Hong <mbxd1@naver.com>
2020-08-16 02:05:06 +0900

    refactor - use 메소드로 공통 미들웨어 제거

--- server/routes/category.js
+++ server/routes/category.js
@@ -4,9 +4,9 @@ const categoryController = require('../controllers/category');
 const { validateCreateCategory } = require('../middlewares/validator');
 const { isValidJwtToken } = require('../middlewares/auth');
 
-router.post('/', isValidJwtToken, validateCreateCategory, categoryController.create);
 router.get('/', categoryController.findAll);
 router.use(isValidJwtToken);
+router.post('/', validateCreateCategory, categoryController.create);
 router.put('/', categoryController.update);
 router.delete('/:id', categoryController.delete);
 
