e8a8f9c84d0c56f284ca7c0e26750a3e4d1026bb
zoomkoding <zoomkoding@gmail.com>
2020-08-15 23:49:33 +0900

    refactor - subcategory page 변동 사항 반영

--- client/src/pages/Subcategory/index.tsx
+++ client/src/pages/Subcategory/index.tsx
@@ -1,40 +1,29 @@
-import SubcategoryTemplate from './template';
+import SubcategoryTemplate from '../../templates/Subcategory';
 import React from 'react';
 
 import ProductCardGrid from '../../components/ProductCardGrid';
 import ProductCardGridHeader from '../../components/ProductCardGridHeader';
-import { Subcategory, ProductSort } from '../../types/Data';
+import { ProductSort } from '../../types/Data';
 import useProducts from '../../hooks/useProducts';
-import { ACTION_FETCH_PRODUCTS } from '../../commons/constants/string';
+import { fetchProducts } from '../../hooks/useProducts/actions';
 
 function SubCategory(): React.ReactElement {
-  const [state, setState] = useProducts({ subcategoryId: 1 });
+  const [productsState, productsDispatch] = useProducts({ subcategoryId: 1 });
+  const { products } = productsState;
 
   function changeSort(sortBy: ProductSort) {
-    setState({
-      type: ACTION_FETCH_PRODUCTS,
-      data: {
+    productsDispatch(
+      fetchProducts({
         subcategoryId: 1,
         ...(sortBy && { sortBy }),
-      },
-    });
-  }
-
-  const { products } = state;
-  if (!products || !products) {
-    return (
-      <SubcategoryTemplate
-        productCardGridHeader={<ProductCardGridHeader changeSort={changeSort} />}
-        productCardGrid={<></>}
-      />
+      }),
     );
   }
 
   return (
     <SubcategoryTemplate
-      //   categoryBanner={<SubCategoryBanner />}
       productCardGridHeader={<ProductCardGridHeader changeSort={changeSort} />}
-      productCardGrid={<ProductCardGrid products={products} />}
+      productCardGrid={products ? <ProductCardGrid products={products} /> : <></>}
     />
   );
 }
