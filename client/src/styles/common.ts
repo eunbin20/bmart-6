fdb211f2031655b23806441ec6c03d894f43db3b
Donguk Hong <mbxd1@naver.com>
2020-08-17 19:14:05 +0900

    [#18] feat - join markup

---
+++ client/src/styles/common.ts
@@ -0,0 +1,4 @@
+export const WOOWA_COLOR = '#2fbeba';
+
+export const GRAY_2 = '#8F8F8F';
+export const GRAY_1 = '#E6E6E6';
--- client/src/templates/Join/index.tsx
+++ client/src/templates/Join/index.tsx
@@ -5,7 +5,15 @@ import * as S from './style';
 function JoinTemplate() {
   return (
     <DefaultTemplate>
-      <S.Header>B mart</S.Header>
+      <S.Header>Bmart</S.Header>
+      <S.InputContainer>
+        <S.Input type="email" name="email" placeholder="이메일" />
+        <S.Input type="text" name="name" placeholder="이름" />
+        <S.Input type="nickname" name="nickname" placeholder="닉네임" />
+        <S.Input type="password" name="password" placeholder="비밀번호" />
+      </S.InputContainer>
+      <S.PushButton>></S.PushButton>
+      <S.Footer />
     </DefaultTemplate>
   );
 }
