85c18a8f7c46e0c89b710789f1b2dcf4b76cb08a
zoomkoding <zoomkoding@gmail.com>
2020-08-19 02:38:05 +0900

    feat - App에 styles reset, font 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import './styles/reset.scss';
+import './styles/fonts.scss';
 import './styles/globalstyle.scss';
 
 import SubcategoryPage from './pages/SubcategoryPage';
