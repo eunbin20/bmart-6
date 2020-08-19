0007d2ca48ff45f98e4c431c7dd893f19b3495d8
zoomkoding <zoomkoding@gmail.com>
2020-08-19 17:23:03 +0900

    feat - product card 2개일때랑 3개일 때 다른 styling 사용하도록 구현

--- client/src/components/ProductCard/style.ts
+++ client/src/components/ProductCard/style.ts
@@ -10,7 +10,18 @@ const fadeIn = keyframes`
   }
 `;
 
-export const LinkWrapper = styled(Link)`
+// interface ColumnCard {
+//   LinkWrapper: React.FunctionComponent;
+//   ContentContainer: React.FunctionComponent;
+//   ImgWrapper: React.FunctionComponent;
+//   Image: React.FunctionComponent;
+//   LikeIconWrapper: React.FunctionComponent;
+//   LikeIcon: React.FunctionComponent;
+//   Title: React.FunctionComponent;
+//   DiscountedRate: React.FunctionComponent;
+// }
+
+const LinkWrapper = styled(Link)`
   display: flex;
   flex-direction: column;
   cursor: pointer;
@@ -19,85 +30,158 @@ export const LinkWrapper = styled(Link)`
   color: var(--black);
 `;
 
-export const ContentContainer = styled.div`
+const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-export const ImgWrapper = styled.div`
-  width: 100%;
-  height: 165px;
-  margin-bottom: 6px;
-`;
+const LikeIcon = styled.svg``;
 
-export const Image = styled.img`
+const Image = styled.img`
   display: block;
   width: 100%;
   height: 100%;
   object-fit: cover;
 `;
 
-export const LikeIcon = styled.svg``;
-
-export const LikeIconWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  bottom: 33px;
-  left: 132px;
-  background: rgba(0, 0, 0, 0.4);
-  border-radius: 50%;
-  height: 26px;
-  width: 26px;
-`;
-
-export const Title = styled.div`
-  font-family: Nanum Gothic;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 10px;
-  line-height: 12px;
-  letter-spacing: -0.05em;
-
-  margin-bottom: 5px;
-`;
-
-export const DiscountedRate = styled.div`
-  font-family: Nanum Gothic;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 9px;
-  line-height: 10px;
-  letter-spacing: -0.1em;
-  margin-right: 1px;
-
-  color: var(--red);
-`;
-export const DiscountedPrice = styled.div`
-  font-family: Nanum Gothic;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 8px;
-  line-height: 9px;
-  letter-spacing: -0.1em;
-  text-decoration-line: line-through;
-  margin-right: 1px;
-
-  color: var(--gray);
-`;
-export const Price = styled.div`
-  font-family: Nanum Gothic;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 10px;
-  line-height: 12px;
-
-  letter-spacing: -0.1em;
-`;
-
-export const PriceWrapper = styled.div`
+const PriceWrapper = styled.div`
   display: flex;
   align-items: center;
   color: var(--black);
 `;
+
+export const TwoColumnCard = {
+  LinkWrapper,
+  ContentContainer,
+  LikeIcon,
+  Image,
+  PriceWrapper,
+  ImgWrapper: styled.div`
+    width: 100%;
+    height: 165px;
+    margin-bottom: 7px;
+  `,
+  LikeIconWrapper: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    float: right;
+
+    bottom: 36px;
+    right: 10px;
+    height: 26px;
+    width: 26px;
+
+    background: rgba(0, 0, 0, 0.4);
+    border-radius: 50%;
+  `,
+  Title: styled.div`
+    font-family: Nanum Gothic;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 10px;
+    line-height: 12px;
+    letter-spacing: -0.05em;
+    margin-bottom: 5px;
+  `,
+  DiscountedRate: styled.div`
+    font-family: Nanum Gothic;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 9px;
+    line-height: 10px;
+    letter-spacing: -0.1em;
+    margin-right: 1px;
+
+    color: var(--red);
+  `,
+  DiscountedPrice: styled.div`
+    font-family: Nanum Gothic;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 8px;
+    line-height: 9px;
+    letter-spacing: -0.1em;
+    text-decoration-line: line-through;
+    margin-right: 1px;
+
+    color: var(--gray);
+  `,
+  Price: styled.div`
+    font-family: Nanum Gothic;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 10px;
+    line-height: 12px;
+
+    letter-spacing: -0.1em;
+  `,
+};
+
+export const ThreeColumnCard = {
+  LinkWrapper,
+  ContentContainer,
+  LikeIcon,
+  Image,
+  PriceWrapper,
+  ImgWrapper: styled.div`
+    width: 100%;
+    height: 106px;
+    margin-bottom: 6px;
+  `,
+  LikeIconWrapper: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    float: right;
+    position: relative;
+
+    bottom: 33px;
+    right: 7px;
+    height: 26px;
+    width: 26px;
+
+    background: rgba(0, 0, 0, 0.4);
+    border-radius: 50%;
+  `,
+  Title: styled.div`
+    font-family: Nanum Gothic;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 10px;
+    line-height: 12px;
+    letter-spacing: -0.05em;
+    margin-bottom: 5px;
+  `,
+  DiscountedRate: styled.div`
+    font-family: Nanum Gothic;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 9px;
+    line-height: 10px;
+    letter-spacing: -0.1em;
+
+    color: var(--red);
+  `,
+  DiscountedPrice: styled.div`
+    font-family: Nanum Gothic;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 8px;
+    line-height: 9px;
+    letter-spacing: -0.1em;
+    text-decoration-line: line-through;
+    margin-right: 1px;
+
+    color: var(--gray);
+  `,
+  Price: styled.div`
+    font-family: Nanum Gothic;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 10px;
+    line-height: 12px;
+    letter-spacing: -0.1em;
+  `,
+};
