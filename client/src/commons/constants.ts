794092a71646d7c71e09d9e84dce4fee3b3e1f0c
zoomkoding <zoomkoding@gmail.com>
2020-08-20 02:31:34 +0900

    feat - SORTOPTION에 타입 추가

--- client/src/commons/constants.ts
+++ client/src/commons/constants.ts
@@ -4,8 +4,10 @@ export const PRICEDOWN = 'pricedown';
 export const NEW = 'new';
 export const DISCOUNTEDRATE = 'discountedRate';
 
-export const SORTOPTIONS = {
-  '기본 정렬순': null,
+export const SORTOPTIONS: {
+  [key: string]: string | undefined;
+} = {
+  '기본 정렬순': undefined,
   '인기 상품순': POPULAR,
   '높은 가격순': PRICEUP,
   '낮은 가격순': PRICEDOWN,
