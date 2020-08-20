f9ac50731faf084c93e21adafd25bf7d3bcbb1ca
zoomkoding <zoomkoding@gmail.com>
2020-08-20 17:48:48 +0900

    feat - 간단한 PageHeader 구현

--- client/src/components/large/PageHeader/index.tsx
+++ client/src/components/large/PageHeader/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import * as S from './style';
+import { Logo } from '../../../commons/svgs';
 
 function PageHeader(): React.ReactElement {
-  return <></>;
+  return (
+    <S.PageHeaderContainer>
+      <S.PageHeader>{Logo()}</S.PageHeader>
+    </S.PageHeaderContainer>
+  );
 }
 
 export default PageHeader;
---
+++ client/src/components/large/PageHeader/style.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const PageHeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 0 15px;
+`;
+export const PageHeader = styled.div`
+  display: flex;
+  width: 100%;
+  height: 52px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const SearchBar = styled.div`
+  border: 1px solid #999999;
+  border-radius: 10px;
+  font-size: 12px;
+  line-height: 14px;
+  color: #222222;
+`;
