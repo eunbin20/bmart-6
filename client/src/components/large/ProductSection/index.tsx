ec82a1949620f19ea9af0981e81ba335b87f396a
zoomkoding <zoomkoding@gmail.com>
2020-08-20 17:33:50 +0900

    feat - ProductSection 구현

--- client/src/components/large/ProductSection/index.tsx
+++ client/src/components/large/ProductSection/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as S from './style';
 import { Product, Header, ProductGridColumns } from '../../../types/data';
 import { SectionHeader, ProductCardGrid, ProductCardListView } from '../..';
 
@@ -11,14 +12,19 @@ interface Props {
 
 function ProductSection({ header, products, viewType, columns }: Props): React.ReactElement {
   return (
-    <>
-      <SectionHeader {...header} />
+    <S.ProductSection>
+      <S.HeaderContainer>{<SectionHeader {...header} />}</S.HeaderContainer>
+
       {viewType === 'grid' ? (
-        <ProductCardGrid products={products} columns={columns} />
+        <S.ProductGridContainer>
+          {<ProductCardGrid products={products} columns={columns} />}
+        </S.ProductGridContainer>
       ) : (
-        <ProductCardListView products={products} columns={columns} />
+        <S.ProductListViewContainer>
+          {<ProductCardListView products={products} columns={columns} />}
+        </S.ProductListViewContainer>
       )}
-    </>
+    </S.ProductSection>
   );
 }
 
---
+++ client/src/components/large/ProductSection/style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const ProductSection = styled.div`
+  display: grid;
+  flex-direction: column;
+  width: 100%;
+  margin: 16px 0;
+`;
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  margin-bottom: 16px;
+`;
+
+export const ProductGridContainer = styled.div`
+  width: 100%;
+  padding: 0 15px;
+`;
+
+export const ProductListViewContainer = styled.div`
+  width: 100%;
+  padding-left: 15px;
+  overflow-x: scroll;
+`;
