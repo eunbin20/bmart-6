454535a1682447907dfdc07c3b49bdd12984a933
Wudy <wudy343@gmail.com>
2020-08-21 12:46:39 +0900

    [#30] feat: HotDealSection 컴포넌트 추가

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,15 +3,17 @@ export { default as ProductCard } from './small/ProductCard';
 export { default as SectionDivider } from './small/SectionDivider';
 export { default as UserHeader } from './small/UserHeader';
 export { default as UserFooter } from './small/UserFooter';
+export { default as HotDealTimer } from './small/HotDealTimer';
+export { default as HotDealBigProductCard } from './small/HotDealBigProductCard';
 
 export { default as PageHeader } from './medium/PageHeader';
 export { default as SectionHeader } from './medium/SectionHeader';
+export { default as BannerSlider } from './medium/BannerSlider';
 export { default as CategoryIconGrid } from './medium/CategoryIconGrid';
 export { default as ProductCardGrid } from './medium/ProductCardGrid';
 export { default as ProductCardGridHeader } from './medium/ProductCardGridHeader';
 export { default as ProductCardListView } from './medium/ProductCardListView';
 export { default as ProductSortModal } from './medium/ProductSortModal';
-export { default as BannerSlider } from './medium/BannerSlider';
 export { default as CategoryProductHeader } from './medium/CategoryProductHeader';
 
 export { default as CategoryProductSection } from './large/CategoryProductSection';
