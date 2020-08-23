36f7697601b0d4ed3cd1e074f2df1e7c9f658142
zoomkoding <zoomkoding@gmail.com>
2020-08-24 02:59:41 +0900

    feat - CategoryPage에서 subcategory 가져오도록 구현

--- client/src/pages/CategoryPage/index.tsx
+++ client/src/pages/CategoryPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './style';
 import DefaultTemplate from '../Default';
 import { ProductCardGridHeader, ProductCardGrid, PageHeader } from '../../components';
@@ -6,16 +6,20 @@ import useProducts from '../../hooks/useProducts';
 import { getProducts } from '../../hooks/useProducts/actions';
 import { SORTOPTIONS } from '../../commons/constants';
 import { RouteComponentProps } from 'react-router-dom';
+import { getSubcategories } from '../../apis/category';
 
 interface Params {
   categoryId: string;
 }
 
 function CategoryPage({ match: { params } }: RouteComponentProps<Params>): React.ReactElement {
-  const [productsState, productDispatch] = useProducts({ categoryId: +params.categoryId });
+  const [subcategories, setSubcategories] = useState([]);
+  const [{ products }, productDispatch] = useProducts({ categoryId: +params.categoryId });
   const [sortBy, setSortBy] = useState('기본 정렬순');
 
-  const { products } = productsState;
+  useEffect(() => {
+    getSubcategories(+params.categoryId).then((res) => setSubcategories(res.data));
+  }, []);
 
   function changeSort(sortBy: string) {
     productDispatch(
