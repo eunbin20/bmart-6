72bbe1f476c6087e34de999abd80067e17e3cd65
zoomkoding <zoomkoding@gmail.com>
2020-08-25 01:04:43 +0900

    [#52] feat - SearchBar UI 구현

---
+++ client/src/components/medium/SearchBar/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import * as S from './style';
+import { Framework7Icon } from '../..';
+
+interface Props {
+  createSearch: Function;
+  history?: any;
+  presetTitle?: string;
+}
+
+function SearchBar({ presetTitle, history, createSearch }: Props): React.ReactElement {
+  const [title, setTitle] = useState(presetTitle ?? '');
+  function onSearchIconClick() {
+    if (title.length == 0) return;
+
+    createSearch({
+      title,
+      createdAt: new Date().toString(),
+    });
+    history?.push(`/search/${title}`);
+  }
+
+  return (
+    <S.SearchBar>
+      <S.IconContainer onClick={() => history?.goBack()}>
+        <Framework7Icon iconName="arrow_left" fontSize={'20px'} />
+      </S.IconContainer>
+      <S.Input
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        placeholder={'어떤 상품을 찾으시나요?'}
+      ></S.Input>
+      <S.IconContainer onClick={() => onSearchIconClick()}>
+        <Framework7Icon iconName="search" fontSize={'20px'} />
+      </S.IconContainer>
+    </S.SearchBar>
+  );
+}
+
+export default SearchBar;
---
+++ client/src/components/medium/SearchBar/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const SearchBar = styled.div`
+  width: 100%;
+  height: 45px;
+  display: flex;
+  align-items: center;
+`;
+
+export const IconContainer = styled.div`
+  padding: 0 14px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  border: none;
+  font-size: 15px;
+`;
