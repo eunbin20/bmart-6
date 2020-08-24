ff7844c28f8f4749fac28a73ea8cf78d7108bf79
zoomkoding <zoomkoding@gmail.com>
2020-08-25 01:11:24 +0900

    [#52] SearchHistory 삭제 기능 구현

--- client/src/pages/SearchPage/index.tsx
+++ client/src/pages/SearchPage/index.tsx
@@ -8,6 +8,26 @@ import { RouteComponentProps } from 'react-router-dom';
 export default function SearchPage(props: RouteComponentProps) {
   const [searches, setSearches] = useState<Search[]>([]);
 
+  function deleteSearch(title?: string) {
+    // deleteAll
+    if (!title) {
+      localStorage.removeItem(RECENT_SEARCH);
+      setSearches([]);
+      return;
+    }
+
+    const index = searches.findIndex((search) => search.title === title);
+    const updatedSearches = [...searches];
+    updatedSearches.splice(index, 1);
+
+    setSearches(updatedSearches);
+    localStorage.setItem(RECENT_SEARCH, JSON.stringify(updatedSearches));
+  }
+
+  useEffect(() => {
+    const searchHistory = localStorage.getItem(RECENT_SEARCH);
+    if (searchHistory) setSearches(JSON.parse(searchHistory));
+  }, [localStorage.getItem(RECENT_SEARCH)]);
   return (
     <S.SearchPage>
       <SearchBar history={props.history} createSearch={createSearch} />
