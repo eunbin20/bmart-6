fceee14871ca7cae12753724d33184b25e3ef72b
zoomkoding <zoomkoding@gmail.com>
2020-08-25 01:12:22 +0900

    [#52] SearchPage Style 구현

---
+++ client/src/pages/SearchPage/style.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const SearchPage = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const SearchResultPage = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const SearchBarContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const ProductCardGridHeaderContainer = styled.div`
+  height: 50px;
+`;
+
+export const ProductCardGridContainer = styled.div`
+  width: 100%;
+  padding: 0 15px;
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding: 0 15px;
+`;
+
+export const NoProductImage = styled.img`
+  width: 100%;
+  margin-right: 20px;
+  margin-bottom: 20px;
+`;
+
+export const NoProductTitle = styled.div`
+  font-weight: 500;
+  font-size: 20px;
+`;
