b451923bcca1ea994670a503d7705aa21b87a1b7
Wudy <wudy343@gmail.com>
2020-08-25 20:04:53 +0900

    [#54] fix: 사파리 scrollTo smooth 미작동 해결
    smoothscroll-polyfill 적용

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import smoothscroll from 'smoothscroll-polyfill';
 import 'framework7-icons';
 import { UserProvider } from './contexts/user';
 import App from './App';
+
+smoothscroll.polyfill();
 
 ReactDOM.render(
   <React.StrictMode>
