204d00e0c4d4ba7815f13e9d8d61ef532580cde3
Donguk Hong <mbxd1@naver.com>
2020-08-26 08:15:05 +0900

    [#57] feat - 텅 빔 페이지 제작

--- client/src/components/large/CartSection/index.tsx
+++ client/src/components/large/CartSection/index.tsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import * as S from './style';
 import activeImage from './aseets/checkbox-active.png';
 import defaultImage from './aseets/checkbox-default.png';
+import { STORAGE_KEY } from '../../../commons/constants';
+import { storage } from '../../../utils/storage';
 
 export default function CartSection() {
+  const [carts, setCarts] = useState(storage.get(STORAGE_KEY.CARTS));
   const [isAllActive, setIsAllActive] = useState(true);
-
   const generateImageByActive = (isActive: boolean) => (isActive ? activeImage : defaultImage);
   const toggleActive = (target: number | 'all') => {
     if (target === 'all') {
@@ -15,14 +17,27 @@ export default function CartSection() {
   };
 
   return (
-    <React.Fragment>
-      <S.SelectManageContainer>
-        <S.ChekBoxContainer>
-          <S.CheckBoxAll background={generateImageByActive(isAllActive)} />
-          <S.CheckAllText onClick={() => toggleActive('all')}>선택해제</S.CheckAllText>
-        </S.ChekBoxContainer>
-        <S.Text>선택 비우기</S.Text>
-      </S.SelectManageContainer>
-    </React.Fragment>
+    <S.CartWrapper>
+      {carts ? (
+        <>
+          <S.SelectManageContainer>
+            <S.ChekBoxContainer>
+              <S.CheckBoxAll background={generateImageByActive(isAllActive)} />
+              <S.CheckAllText onClick={() => toggleActive('all')}>선택해제</S.CheckAllText>
+            </S.ChekBoxContainer>
+            <S.Text>선택 비우기</S.Text>
+          </S.SelectManageContainer>
+          <S.MainContainer>
+            <S.Title>장바구니</S.Title>
+            <S.ItemContainer></S.ItemContainer>
+          </S.MainContainer>
+        </>
+      ) : (
+        <S.EmptyContainer>
+          <S.EmptyImage src="/assets/cart-empty.png" />
+          <S.EmptyText>장바구니가 텅 비어있어요🤔🤔🤔🤔</S.EmptyText>
+        </S.EmptyContainer>
+      )}
+    </S.CartWrapper>
   );
 }
--- client/src/components/large/CartSection/style.ts
+++ client/src/components/large/CartSection/style.ts
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
+export const CartWrapper = styled.div`
+  width: 100%;
+`;
+
 export const Text = styled.div`
   font-size: 14px;
   margin-top: 3px;
+  min-width: 61px;
 `;
 
 export const SelectManageContainer = styled.div`
@@ -11,6 +16,7 @@ export const SelectManageContainer = styled.div`
   align-items: center;
   padding: 16px 14px;
   border-bottom: 3px solid var(--border-gray);
+  width: 100%;
 `;
 
 interface CheckBoxAllProps {
@@ -20,6 +26,7 @@ interface CheckBoxAllProps {
 export const ChekBoxContainer = styled.div`
   display: flex;
   align-items: center;
+  width: 100%;
 `;
 
 export const CheckBoxAll = styled.input.attrs({
@@ -39,4 +46,37 @@ export const CheckAllText = styled.label.attrs((props) => ({
   margin-left: 8px;
   font-weight: 600;
   font-size: 14px;
+`;
+
+export const MainContainer = styled.div`
+  padding: 24px 14px;
+`;
+
+export const Title = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  color: var(--green);
+  border-bottom: 2px solid var(--green);
+  padding-bottom: 2px;
+`;
+
+export const ItemContainer = styled.div`
+  padding: 24px 14px;
+`;
+
+export const EmptyContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: calc(100vh - 52px);
+`;
+
+export const EmptyImage = styled.img.attrs((props) => ({
+  src: props.src,
+}))``;
+
+export const EmptyText = styled.div`
+  font-size: 16px;
+  margin-top: 20px;
 `;
