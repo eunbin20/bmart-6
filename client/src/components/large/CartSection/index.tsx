dbc29cfcc7e2db38b17e6d115f23728e429a8805
Donguk Hong <mbxd1@naver.com>
2020-08-26 07:06:06 +0900

    [#57] feat - checkbox render, toggle 기능 구현

--- client/src/components/large/CartSection/index.tsx
+++ client/src/components/large/CartSection/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
+import activeImage from './aseets/checkbox-active.png';
+import defaultImage from './aseets/checkbox-default.png';
 
 export default function CartSection() {
+  const [isAllActive, setIsAllActive] = useState(true);
+
+  const generateImageByActive = (isActive: boolean) => (isActive ? activeImage : defaultImage);
+  const toggleActive = (target: number | 'all') => {
+    if (target === 'all') {
+      setIsAllActive(!isAllActive);
+      return;
+    }
+  };
+
   return (
     <>
-      <S.SelectManageContainer></S.SelectManageContainer>
+      <S.SelectManageContainer>
+        <S.CheckBoxAll background={generateImageByActive(isAllActive)} />
+        <S.CheckAllText onClick={() => toggleActive('all')}>선택해제</S.CheckAllText>
+      </S.SelectManageContainer>
     </>
   );
 }
--- client/src/components/large/CartSection/style.ts
+++ client/src/components/large/CartSection/style.ts
@@ -1,3 +1,27 @@
 import styled from 'styled-components';
 
-export const SelectManageContainer = styled.div``;
+export const SelectManageContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+interface CheckBoxAllProps {
+  background: string;
+}
+
+export const CheckBoxAll = styled.input.attrs({
+  id: 'cart-checkobx-all',
+  type: 'checkbox',
+})`
+  background-image: ${(props: CheckBoxAllProps) => `url(${props.background})`};
+  width: 20px;
+  height: 20px;
+`;
+
+export const CheckAllText = styled.label.attrs((props) => ({
+  for: 'cart-checkbox-all',
+  onClick: props.onClick,
+}))`
+  margin-left: 30px;
+`;
