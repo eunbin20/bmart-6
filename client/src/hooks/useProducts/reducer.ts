82a1d585695b86df8eef2f1e4b6f7b1f638860ed
zoomkoding <zoomkoding@gmail.com>
2020-08-26 21:44:54 +0900

    [#60] feat - 좋아요 시 products 상태 처리 reducer 구현

--- client/src/hooks/useProducts/reducer.ts
+++ client/src/hooks/useProducts/reducer.ts
@@ -1,6 +1,21 @@
+import { Product } from '../../types/data';
 import { ProductsState } from '../../types/states';
 import { ProductsAction } from '../../types/actions';
-import { ACTION_GET_PRODUCTS, ACTION_GET_MORE_PRODUCTS, ACTION_ERROR } from './actions';
+import {
+  ACTION_GET_PRODUCTS,
+  ACTION_GET_MORE_PRODUCTS,
+  ACTION_ERROR,
+  ACTION_LIKE_PRODUCT,
+  ACTION_UNLIKE_PRODUCT,
+} from './actions';
+
+function getProductsAfterLike(products: Product[], productId: number, isLiked: boolean) {
+  const newProducts = [...products];
+  const index = newProducts.findIndex((product) => product.id === productId);
+  newProducts[index].isLiked = isLiked;
+  console.log(newProducts);
+  return newProducts;
+}
 
 export function productsReducer(state: ProductsState, action: ProductsAction): ProductsState {
   switch (action.type) {
@@ -18,6 +33,22 @@ export function productsReducer(state: ProductsState, action: ProductsAction): P
         status: action.value.status,
       };
 
+    case ACTION_LIKE_PRODUCT:
+      if (!state.products || !action.value.productId) return state;
+      console.log('hi');
+      console.log(state);
+      console.log(action.value);
+      return {
+        products: getProductsAfterLike(state.products, action.value.productId, true),
+        status: action.value.status,
+      };
+
+    case ACTION_UNLIKE_PRODUCT:
+      if (!state.products || !action.value.productId) return state;
+      return {
+        products: getProductsAfterLike(state.products, action.value.productId, false),
+        status: action.value.status,
+      };
     case ACTION_ERROR:
       return {
         ...state,
