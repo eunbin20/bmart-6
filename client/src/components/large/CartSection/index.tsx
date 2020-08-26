a6f82a76e710f5d8908a4ad453343ecc707e8247
Donguk Hong <mbxd1@naver.com>
2020-08-26 09:24:19 +0900

    [#57] feat - 장바구니 item markup

--- client/src/components/large/CartSection/index.tsx
+++ client/src/components/large/CartSection/index.tsx
@@ -5,11 +5,14 @@ import defaultImage from './aseets/checkbox-default.png';
 import { STORAGE_KEY } from '../../../commons/constants';
 import { storage } from '../../../utils/storage';
 import { Empty } from '../../../components';
+import QuantityCounter from '../../small/QuantityCoutner';
 
 export default function CartSection() {
   const [carts, setCarts] = useState(storage.get(STORAGE_KEY.CARTS));
   const [isAllActive, setIsAllActive] = useState(true);
+
   const generateImageByActive = (isActive: boolean) => (isActive ? activeImage : defaultImage);
+
   const toggleActive = (target: number | 'all') => {
     if (target === 'all') {
       setIsAllActive(!isAllActive);
@@ -17,20 +20,51 @@ export default function CartSection() {
     }
   };
 
+  const setCount = () => {};
+
   return (
     <S.CartWrapper>
       {carts ? (
         <>
           <S.SelectManageContainer>
             <S.ChekBoxContainer>
-              <S.CheckBoxAll background={generateImageByActive(isAllActive)} />
+              <S.CheckBox
+                onClick={() => toggleActive('all')}
+                id="cart-checkobx-all"
+                background={generateImageByActive(isAllActive)}
+              />
               <S.CheckAllText onClick={() => toggleActive('all')}>선택해제</S.CheckAllText>
             </S.ChekBoxContainer>
             <S.Text>선택 비우기</S.Text>
           </S.SelectManageContainer>
           <S.MainContainer>
             <S.Title>장바구니</S.Title>
-            <S.ItemContainer></S.ItemContainer>
+            <S.ItemContainer>
+              <S.ItemWrapper>
+                <S.HeaderBox>
+                  <S.CheckBox
+                    onClick={() => toggleActive('all')}
+                    id="cart-checkobx-1"
+                    background={generateImageByActive(isAllActive)}
+                  />
+                  <S.HeaderText id="cart-checkobx-1">CJ 유부초밥 맛있고 짠 초밥</S.HeaderText>
+                </S.HeaderBox>
+                <S.ContentBox>
+                  <S.ContentImage src="" />
+                  <S.ContentPriceBox>
+                    <S.Price>(3,000원)</S.Price>
+                    <S.DiscountedPriceWrapper>
+                      <S.StrikePrice>3,000원</S.StrikePrice>
+                      <S.DiscountedPrice>4,900원</S.DiscountedPrice>
+                    </S.DiscountedPriceWrapper>
+                    <S.QuantityCounterWrapper>
+                      <QuantityCounter count={1} setCount={setCount} />
+                    </S.QuantityCounterWrapper>
+                  </S.ContentPriceBox>
+                  <S.ItemDeleteButton onClick={() => {}}>삭제</S.ItemDeleteButton>
+                </S.ContentBox>
+              </S.ItemWrapper>
+            </S.ItemContainer>
           </S.MainContainer>
         </>
       ) : (
--- client/src/components/large/CartSection/style.ts
+++ client/src/components/large/CartSection/style.ts
@@ -10,6 +10,22 @@ export const Text = styled.div`
   min-width: 61px;
 `;
 
+interface CheckBoxProps {
+  id: string;
+  background: string;
+  onClick: (target: number | 'all') => void;
+}
+
+export const CheckBox = styled.input.attrs((props) => ({
+  id: props.id,
+  onClick: props.onClick,
+  type: 'checkbox',
+}))`
+  background-image: ${(props: CheckBoxProps) => `url(${props.background})`};
+  width: 20px;
+  height: 20px;
+`;
+
 export const SelectManageContainer = styled.div`
   display: flex;
   justify-content: space-between;
@@ -19,31 +35,18 @@ export const SelectManageContainer = styled.div`
   width: 100%;
 `;
 
-interface CheckBoxAllProps {
-  background: string;
-}
-
 export const ChekBoxContainer = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
 `;
 
-export const CheckBoxAll = styled.input.attrs({
-  id: 'cart-checkobx-all',
-  type: 'checkbox',
-})`
-  background-image: ${(props: CheckBoxAllProps) => `url(${props.background})`};
-  width: 20px;
-  height: 20px;
-`;
-
 export const CheckAllText = styled.label.attrs((props) => ({
-  for: 'cart-checkbox-all',
+  htmlFor: 'cart-checkbox-all',
   onClick: props.onClick,
 }))`
   margin-top: 3px;
-  margin-left: 8px;
+  margin-left: 14px;
   font-weight: 600;
   font-size: 14px;
 `;
@@ -61,5 +64,87 @@ export const Title = styled.span`
 `;
 
 export const ItemContainer = styled.div`
-  padding: 24px 14px;
+  padding: 24px 0px;
+`;
+
+export const ItemWrapper = styled.div``;
+
+export const HeaderBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const HeaderText = styled.label.attrs((props) => ({
+  htmlFor: props.id,
+}))`
+  font-size: 18px;
+  font-weight: 700;
+  margin-left: 14px;
+  margin-top: 4px;
+`;
+
+export const ContentBox = styled.div`
+  display: flex;
+  margin-top: 14px;
+`;
+
+interface ContentImage {
+  src: string;
+}
+
+export const ContentImage = styled.image.attrs((props: ContentImage) => ({
+  src: props.src,
+  alt: 'content-image',
+}))`
+  min-width: 106px;
+  min-height: 106px;
+  background: var(--gray);
+`;
+
+export const ContentPriceBox = styled.div`
+  margin-left: 14px;
+  position: relative;
+`;
+
+export const Price = styled.div`
+  color: var(--strong-gray);
+  font-size: 15px;
+`;
+
+export const DiscountedPriceWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const StrikePrice = styled.div.attrs({
+  className: 'strike',
+})`
+  color: var(--gray);
+  font-size: 15px;
+  margin-right: 5px;
+`;
+
+export const DiscountedPrice = styled.div`
+  font-weight: 700;
+  font-size: 17px;
+`;
+
+export const QuantityCounterWrapper = styled.div`
+  margin-top: auto;
+  position: absolute;
+  bottom: 0;
+`;
+
+export const ItemDeleteButton = styled.button.attrs((props) => ({
+  onClick: props.onClick,
+}))`
+  outline: none;
+  border: none;
+  color: var(--green);
+  font-size: 15px;
+  font-weight: 700;
+  min-width: 41px;
+  height: 1px;
+  background-color: var(--white);
+  margin-left: auto;
 `;
