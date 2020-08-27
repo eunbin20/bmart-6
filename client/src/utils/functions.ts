6dff5e84ac7cd53e044377b6e1ab0e4c8984d327
zoomkoding <zoomkoding@gmail.com>
2020-08-27 22:32:44 +0900

    [#84] feat - location.state에 전 location 정보 저장하는 함수 구현

--- client/src/utils/functions.ts
+++ client/src/utils/functions.ts
@@ -1,2 +1,6 @@
 export const makeComma = (price: number) => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 export const delay = (ms: number) => new Promise((resolve) => setTimeout(() => resolve(), ms));
+export const setLink = (pathname: string, location: any) => ({
+  pathname,
+  state: { from: location },
+});
