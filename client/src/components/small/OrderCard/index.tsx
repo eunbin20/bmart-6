81d1d9bf4fad36aba2c73e8564044bf12b98c128
Donguk Hong <mbxd1@naver.com>
2020-08-28 00:50:13 +0900

    [#88] feat - 검색바 > Enter로 검색하기

--- client/src/components/small/OrderCard/index.tsx
+++ client/src/components/small/OrderCard/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as S from './style';
 import { Order } from '../../../types/data';
 import { formatDate } from '../../../utils/functions';
+import { makeComma } from '../../../utils/functions';
 
 interface Props {
   order: Order;
@@ -16,7 +17,7 @@ function OrderCard({ order }: Props): React.ReactElement {
         <S.TitleContainer>
           {products[0].title} {products.length != 1 && `외 ${products.length - 1}개`}
         </S.TitleContainer>
-        <S.PriceContainer>{totalDiscountedPrice}원</S.PriceContainer>
+        <S.PriceContainer>{makeComma(totalDiscountedPrice)}원</S.PriceContainer>
       </S.DataContainer>
       <S.ImageContainer>
         <S.Image src={products[0].imageUrl}></S.Image>
