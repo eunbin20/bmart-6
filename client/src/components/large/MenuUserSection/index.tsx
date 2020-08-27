256db36306fdea4ed95f13a2b912e1a8eb5b19e8
Wudy <wudy343@gmail.com>
2020-08-27 21:51:19 +0900

    refactor: 의미 없는 console.log 제거

--- client/src/components/large/MenuUserSection/index.tsx
+++ client/src/components/large/MenuUserSection/index.tsx
@@ -3,13 +3,25 @@ import * as S from './style';
 import { Framework7Icon } from '../../../components';
 import { useAuthContext } from '../../../contexts/user/index';
 
+const getSuggestLink = (isAuthorized: boolean) => {
+  return isAuthorized
+    ? {
+        url: '/user/logout',
+        text: '로그아웃 하기',
+      }
+    : {
+        url: '/user/login',
+        text: '로그인 하기',
+      };
+};
+
 function MenuUserSection(): React.ReactElement {
   const authContext = useAuthContext();
-
+  const suggestLink = getSuggestLink(authContext?.state.isAuthorized || false);
   return (
     <S.SectionContainer>
-      <S.SuggestLink to="/user/login">
-        {authContext?.state.isAuthorized ? '로그아웃 하기' : '로그인 하기'}
+      <S.SuggestLink to={suggestLink.url}>
+        {suggestLink.text}
         <Framework7Icon iconName="chevron_right" fontSize={'12px'} />
       </S.SuggestLink>
       <S.UserMenuContainer>
