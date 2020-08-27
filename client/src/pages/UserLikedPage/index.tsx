1b9130f16c56a28b1bad175f06e5b15299528e30
Wudy <wudy343@gmail.com>
2020-08-27 16:47:32 +0900

    [#61] feat: 찜한상품 페이지 구현

--- client/src/pages/UserLikedPage/index.tsx
+++ client/src/pages/UserLikedPage/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import * as S from './style';
 import DefaultTemplate from '../Default';
-import { ProductCardGrid, PageHeader, SectionHeader, SectionDivider } from '../../components';
-import useProducts from '../../hooks/useProducts';
+import {
+  ProductCardGrid,
+  PageHeader,
+  SectionHeader,
+  SectionDivider,
+  Empty,
+} from '../../components';
+import useProducts, { FILTER_TYPE } from '../../hooks/useProducts';
 import { Header } from '../../types/data';
 
-function CategoryPage(): React.ReactElement {
-  const [{ products }] = useProducts({});
+function UserLikedPage({ history }: RouteComponentProps): React.ReactElement {
+  const [{ products }] = useProducts({ type: FILTER_TYPE.USER_LIKED });
   const sectionHeaderProps: Header = {
     title: '찜한상품',
     description: <S.ProductCount>{products ? products.length : '0'}개</S.ProductCount>,
@@ -18,10 +25,14 @@ function CategoryPage(): React.ReactElement {
       <SectionDivider />
       <S.HeaderContainer>{<SectionHeader {...sectionHeaderProps} />}</S.HeaderContainer>
       <S.ProductCardGridContainer>
-        {products && <ProductCardGrid products={products} columns={2} />}
+        {products && products.length > 0 ? (
+          <ProductCardGrid products={products} columns={2} />
+        ) : (
+          <Empty subtractHeight="52" text="찜한상품이 없어요..." />
+        )}
       </S.ProductCardGridContainer>
     </DefaultTemplate>
   );
 }
 
-export default CategoryPage;
+export default UserLikedPage;
