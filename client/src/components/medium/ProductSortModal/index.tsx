cdc9dd4cbd846679045aed5d7a88ef680846e31b
Wudy <wudy343@gmail.com>
2020-08-28 03:44:58 +0900

    [#100] feat: 상품 정렬 모달, 높이 제거 및 bottom padding

--- client/src/components/medium/ProductSortModal/index.tsx
+++ client/src/components/medium/ProductSortModal/index.tsx
@@ -25,14 +25,13 @@ const customStyle = {
 
   content: {
     width: '100%',
-    height: '93.333vw',
     top: 'auto',
     right: 'auto',
     left: 'auto',
     bottom: 0,
     background: 'var(--white)',
     borderRadius: '4vw 4vw 0vw 0vw',
-    padding: 0,
+    padding: '0 0 2vw 0',
   },
 };
 
